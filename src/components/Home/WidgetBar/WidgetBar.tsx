d7e197ffb811b695c63d219d09e9d441932d80f7
SudoLemuel <lembak03@hotmail.com>
2023-05-28 22:55:08 +0200

    :art: prettier

--- src/components/Home/WidgetBar/WidgetBar.tsx
+++ src/components/Home/WidgetBar/WidgetBar.tsx
@@ -1,74 +1,76 @@
-import { IconButton } from "@mui/material";
-import router from "next/router";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import TodayIcon from '@mui/icons-material/Today';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
-import AccessTimeIcon from '@mui/icons-material/AccessTime';
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { IconButton } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 export const WidgetBar = () => {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=" + process.env.NEXT_PUBLIC_WEATHER_KEY;
+    const url =
+        'https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=' +
+        process.env.NEXT_PUBLIC_WEATHER_KEY;
     const [weather, setWeather] = useState(null);
     useEffect(() => {
         axios.get(url).then((response) => {
             setWeather(response.data.main.temp);
-
         });
     }, []);
     console.log(weather);
 
-  
-   
-    const currentDate = new Date().toLocaleDateString( [], { weekday: 'short', month: 'long', day: 'numeric' });
-    const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'} );
+    const currentDate = new Date().toLocaleDateString([], {
+        weekday: 'short',
+        month: 'long',
+        day: 'numeric',
+    });
+    const currentTime = new Date().toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+    });
     const [time, setTime] = useState(currentTime);
     useEffect(() => {
         const interval = setInterval(() => {
-            
-            setTime(new Date().toLocaleTimeString( [], { hour: '2-digit', minute: '2-digit'}));
+            setTime(
+                new Date().toLocaleTimeString([], {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                })
+            );
         }, 1000);
         return () => clearInterval(interval);
-        
     }, []);
-
-
 
     return (
         <div>
             <div className="flex justify-center">
-            <div className=" w-11/12 r bg-darker p-1.5 rounded-lg shadow " >
-                <div className="flex justify-evenly">
-                    <IconButton aria-label="calendar" size="small">
-                        <TodayIcon
-                            className={
-                                `h-6 w-6 text-light` 
-                            }
-                        />
-                        <span className="text-white text-sm"> {currentDate} </span>
-                    </IconButton>
+                <div className=" r w-11/12 rounded-lg bg-darker p-1.5 shadow ">
+                    <div className="flex justify-evenly">
+                        <IconButton aria-label="calendar" size="small">
+                            <TodayIcon className={`h-6 w-6 text-light`} />
+                            <span className="text-sm text-white">
+                                {' '}
+                                {currentDate}{' '}
+                            </span>
+                        </IconButton>
 
-                    <IconButton aria-label="sun" size="small">
-                        <WbSunnyIcon
-                            className={
-                                `h-6 w-6 text-light`
-                            }
-                        />
-                        <span className="text-white text-sm"> {weather} </span>
-                    </IconButton>
-                    <IconButton aria-label="clock" size="small">
-                        <AccessTimeIcon
-                            className={
-                                `h-6 w-6 text-light`
-                            }
-                        />
+                        <IconButton aria-label="sun" size="small">
+                            <WbSunnyIcon className={`h-6 w-6 text-light`} />
+                            <span className="text-sm text-white">
+                                {' '}
+                                {weather}{' '}
+                            </span>
+                        </IconButton>
+                        <IconButton aria-label="clock" size="small">
+                            <AccessTimeIcon className={`h-6 w-6 text-light`} />
 
-                        <span className="text-white text-sm"> {time} °C   </span>
-                    </IconButton>
-
+                            <span className="text-sm text-white">
+                                {' '}
+                                {time} °C{' '}
+                            </span>
+                        </IconButton>
                     </div>
-            </div>
+                </div>
             </div>
         </div>
     );
 };
-    
+
 export default WidgetBar;
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,4 @@
-
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
 // colors: {
 //     primary: '#B8ED91',
@@ -12,39 +11,39 @@ import { createTheme } from "@mui/material/styles";
 // },
 
 export const theme = createTheme({
- palette: {
-   primary: {
-        main: "#B8ED91",
-        light: "#B8ED91",
-        dark: "#B8ED91",
-        contrastText: "#FFFFFF",
-   },
-    secondary: {
-        main: "#BF616A",
-        light: "#BF616A",
-        dark: "#BF616A",
-        contrastText: "#FFFFFF",
+    palette: {
+        primary: {
+            main: '#B8ED91',
+            light: '#B8ED91',
+            dark: '#B8ED91',
+            contrastText: '#FFFFFF',
+        },
+        secondary: {
+            main: '#BF616A',
+            light: '#BF616A',
+            dark: '#BF616A',
+            contrastText: '#FFFFFF',
+        },
+        error: {
+            main: '#BF616A',
+            light: '#BF616A',
+            dark: '#BF616A',
+            contrastText: '#FFFFFF',
+        },
+        warning: {
+            main: '#EBCB8B',
+            light: '#EBCB8B',
+            dark: '#EBCB8B',
+            contrastText: '#FFFFFF',
+        },
+        text: {
+            primary: '#ffffff',
+            secondary: '#ffffff',
+            disabled: '#ffffff',
+        },
+        background: {
+            paper: '#181E2A',
+            default: '#181E2A',
+        },
     },
-    error: {
-        main: "#BF616A",
-        light: "#BF616A",
-        dark: "#BF616A",
-        contrastText: "#FFFFFF",
-    },
-    warning: {
-        main: "#EBCB8B",
-        light: "#EBCB8B",
-        dark: "#EBCB8B",
-        contrastText: "#FFFFFF",
-    },
-    text: {
-        primary: "#ffffff",
-        secondary: "#ffffff",
-        disabled: "#ffffff"
-    },
-    background: {
-        paper: "#181E2A",
-        default: "#181E2A",
-    },
- },
 });
